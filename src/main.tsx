a2116b802a1131e1bfb3eda14f6dae42f6e74169
HP486379 <soffio2@gmail.com>
2026-06-06 18:14:28 +0900

    Wire team name suffix controls

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { initEntrySubmit } from './entry-submit-wireup';
 import { initContestTicker } from './contest-ticker-wireup';
 import { initFormationMiniLayout } from './formation-mini-layout-wireup';
 import { initLowerLayoutWireup } from './lower-layout-wireup';
+import { initTeamNameSuffixWireup } from './team-name-suffix-wireup';
 import './styles.css';
 import './market-data.css';
 import './layout-fixes.css';
@@ -20,6 +21,7 @@ import './contest-ticker.css';
 import './contest-ticker-neon.css';
 import './contest-layout-cleanup.css';
 import './lower-layout-fixes.css';
+import './team-name-suffix.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -33,3 +35,4 @@ initEntrySubmit();
 initContestTicker();
 initFormationMiniLayout();
 initLowerLayoutWireup();
+initTeamNameSuffixWireup();
