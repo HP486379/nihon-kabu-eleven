5bd6dcfad5c8e98d7b7228ca8034558c2b571147
HP486379 <soffio2@gmail.com>
2026-06-08 06:34:09 +0900

    Wire results page into app

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import { initMatchDurationRules } from './match-duration-rules-wireup';
 import { initContestListPage } from './contest-list-page-wireup';
 import { initFormationPage } from './formation-page-wireup';
 import { initParticipantsPage } from './participants-page-wireup';
+import { initResultsPage } from './results-page-wireup';
 import './styles.css';
 import './market-data.css';
 import './layout-fixes.css';
@@ -29,6 +30,7 @@ import './match-duration-rules.css';
 import './contest-list-page.css';
 import './formation-page.css';
 import './participants-page.css';
+import './results-page.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -47,3 +49,4 @@ initMatchDurationRules();
 initContestListPage();
 initFormationPage();
 initParticipantsPage();
+initResultsPage();
